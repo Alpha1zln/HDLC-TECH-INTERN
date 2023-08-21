f560ee1ebed12495aad842199b122f2f1b0ab0a0
Alpha1zln <98889077+Alpha1zln@users.noreply.github.com>
2023-08-22 02:13:06 +0530

    Create script.js

---
+++ ASSIGNMENT 3/script.js
@@ -0,0 +1,17 @@
+function performOperation(operator) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    let result;
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else if (operator === '*') {
+        result = num1 * num2;
+    } else if (operator === '/') {
+        result = num1 / num2;
+    }
+
+    document.getElementById("result").textContent = `Result: ${result}`;
+}
