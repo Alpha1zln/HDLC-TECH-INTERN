f9abd1d9614d7a9c8939fd5bbca91c2ea2744a14
Alpha1zln <98889077+Alpha1zln@users.noreply.github.com>
2023-08-24 23:32:50 +0530

    Update app.components.ts

--- ASSIGNMENT 4/components/app.components.ts
+++ ASSIGNMENT 4/components/app.components.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-root',
-    template: `<div>
-                    <h1> {{FirstName + ' ' + LastName}} </h1>                 
-               </div>`
-})
+// import { Component } from '@angular/core';
+// @Component({
+//   selector: 'app-root',
+//     template: `<div>
+//                     <h1> {{FirstName + ' ' + LastName}} </h1>                 
+//                </div>`
+// })
 
-export class AppComponent {
-  FirstName: string = 'Shekhar';
-  LastName: string = 'Mohan';
-}
+// export class AppComponent {
+//   FirstName: string = 'Shekhar';
+//   LastName: string = 'Mohan';
+// }
+
+
+import { Component } from "@angular/core";  
+@Component({  
+    selector: 'my-App',  
+    template: `  
+                <div>  
+                <strong>{{firstname}}</strong>  
+                 <strong>{{lastname}}</strong>  
+                </div>  
+})  
+export class AppComponent {  
+    firstname: string = "Sachin";  
+    lastname:string = "Tendulkar"  
